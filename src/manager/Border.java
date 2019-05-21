718bab76ec506324c36be916f357e548c96e5283
Ilya <ilya151098@yandex.ru>
2019-05-22 01:29:32 +0300

    realised class border

--- src/manager/Border.java
+++ src/manager/Border.java
@@ -1,4 +1,21 @@
 package manager;
 
+import static manager.Manager.SCREEN_SIZE_X;
+import static manager.Manager.SCREEN_SIZE_Y;
+
 public class Border {
+    private int x1, x2, y1, y2;
+    void changer(int xo1, int xo2, int yo1, int yo2) {
+        x1 = (x2 - x1) / SCREEN_SIZE_X * xo1 + x1;
+        x2 = (x2 - x1) / SCREEN_SIZE_X * xo2 + x1;
+        y1 = (y2 - y1) / SCREEN_SIZE_Y * yo1 + y1;
+        y2 = (y2 - y1) / SCREEN_SIZE_Y * yo2 + y1;
+    }
+
+    public Border(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
 }
--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -1,6 +1,8 @@
 package manager;
 
 public class Manager {
+    public static int  SCREEN_SIZE_X = 1000000;
+    public static int SCREEN_SIZE_Y = 700000;
     public static void main(String[] args){
 
     }
